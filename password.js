6d5b64a5053e0ce6cd1ab9594eaa4cdc94697ab8
h0lm3 <patrick.holmes@heiltsuk.ca>
2023-12-06 16:14:54 -0800

    password.js

--- password.js
+++ password.js
@@ -1,20 +1,23 @@
 function obfuscate(inputString) {
-    let obfuscatedString = inputString.replace(/a/g, '4');
-  
-    obfuscatedString = obfuscatedString.replace(/e/g, '3');
-  
-    obfuscatedString = obfuscatedString.replace(/o/g, '0');
-  
-    obfuscatedString = obfuscatedString.replace(/l/g, '1');
-  
-    return obfuscatedString;
+  function replaceCharacters(str, charMap) {
+    for (const [original, replacement] of charMap) {
+      str = str.replace(new RegExp(original, 'g'), replacement);
+    }
+    return str;
   }
-  
-  const originalString = 'Hello, world!';
-  const obfuscatedResult = obfuscate(originalString);
-  console.log('Original string:', originalString);
-  console.log('Obfuscated string:', obfuscatedResult);
-  
-  git clone "https://gist.github.com/h0lm3/8c075bb36c0de0525b71acf737547355.js"
 
-  C:\Users\bethi\Bootcamp\varant\8c075bb36c0de0525b71acf737547355
+  const charMap = [
+    ['a', '4'],
+    ['e', '3'],
+    ['o', '0'],
+    ['l', '1']
+  ];
+
+  return replaceCharacters(inputString, charMap);
+}
+
+const originalString = 'Hello, world!';
+const obfuscatedResult = obfuscate(originalString);
+
+console.log('Original string:', originalString);
+console.log('Obfuscated string:', obfuscatedResult);

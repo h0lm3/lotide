39249d897fb34cc68bb982fd3947e13e10d5c9da
h0lm3 <patrick.holmes@heiltsuk.ca>
2023-12-31 03:38:03 -0800

    https://github.com/h0lm3/lotide.git

--- index.js
+++ index.js
@@ -6,19 +6,9 @@ const eqArrays  = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 
 module.exports = {
-  head:      head,
-  tail:      tail,
-  middle:    middle,
-  eqArrays:  eqArrays,
-  eqObjects: eqObjects,
-};
-// index.js
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
-
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  eqArrays,
+  eqObjects,
 };
